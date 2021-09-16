7444d7de1795055057dc0192b1d168c7f7b954f1
kokinz <nes-t@mail.ru>
2021-09-17 02:45:22 +0300

    fix

--- project/src/components/footer/footer.jsx
+++ project/src/components/footer/footer.jsx
@@ -5,28 +5,28 @@ function Footer() {
     <footer className="footer">
       <div className="footer-wrapper container">
 
-        <nav class="footer__nav nav">
-          <ul class="footer__list nav__list list">
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Корпоративным клиентам</a>
+        <nav className="footer__nav nav">
+          <ul className="footer__list nav__list list">
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Корпоративным клиентам</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Клиентам</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Клиентам</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Аренда авто</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Аренда авто</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Каршеринг</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Каршеринг</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Как продать авто</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Как продать авто</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Trade-in</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Trade-in</a>
             </li>
-            <li class="nav__item footer__item">
-              <a class="nav__link link" href="/#">Test drive</a>
+            <li className="nav__item footer__item">
+              <a className="nav__link link" href="/#">Test drive</a>
             </li>
           </ul>
         </nav>
