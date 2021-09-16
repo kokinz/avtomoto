7f6ee5062a62e75b7c1d78d86d2d702fd00cd4a5
kokinz <nes-t@mail.ru>
2021-09-16 23:47:58 +0300

    Подготовка

---
+++ project/src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss';
+import '../../sass/fonts.scss';
+
+function App() {
+  return (
+    <header className="page-header">
+
+      <div className="copyright">
+        <p>© 2019 What to watch Ltd.</p>
+      </div>
+    </header>
+  );
+}
+
+export default App;
